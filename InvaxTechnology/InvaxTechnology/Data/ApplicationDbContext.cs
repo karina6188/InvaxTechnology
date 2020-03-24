933a2faa27a12ce8596cf44bab41fa1d446c4e30
Karina Chen <karina6188@gmail.com>
2020-03-24 16:04:53 -0700

    application user set up

---
+++ InvaxTechnology/InvaxTechnology/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvaxTechnology.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
