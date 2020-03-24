f8ac5c13ba703961811e8fda7c5efe2fd5d8dcdb
Karina Chen <karina6188@gmail.com>
2020-03-24 15:58:53 -0700

    architect website structure

--- InvaxTechnology/InvaxTechnology/Startup.cs
+++ InvaxTechnology/InvaxTechnology/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,22 +13,49 @@ namespace InvaxTechnology
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public IHostEnvironment Environment { get; }
 
         public Startup(IHostEnvironment environment)
         {
             Environment = environment;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddRazorPages();
+
+            services.AddControllersWithViews();
+
+            string userConnString = Environment.IsDevelopment()
+                ? Configuration["ConnectionStrings:UserConnection"]
+                : Configuration["ConnectionStrings:UserProductionConnection"];
+
+            string productConnString = Environment.IsDevelopment()
+                ? Configuration["ConnectionStrings:ProductConnection"]
+                : Configuration["ConnectionStrings:ProductProductionConnection"];
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(userConnString));
+
+            services.AddDbContext<StoreDbContext>(options =>
+                options.UseSqlServer(productConnString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            options.AddPolicy("AdminOnly", policy => policy.RequireRole(ApplicationRoles.Admin)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -38,10 +66,17 @@ namespace InvaxTechnology
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
+
+            RoleInitializer.SeedData(serviceProvider);
         }
     }
 }
